ba5ff40977eeeb670784f79ecdbe1e0af22cb493
ducquang2 <quangbilun@gmail.com>
2020-10-13 11:07:40 +0700

    boring-method

--- hackerrank-learning/conditional-if-else.cpp
+++ hackerrank-learning/conditional-if-else.cpp
@@ -10,6 +10,38 @@ int main()
     cin >> n;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     
+    // Enter your code here
     
+    /* Such method
+    if (n > 9) {
+        cout << "Greater than 9";
+    }
+    else if (n == 1) {
+        cout << "one";
+    }
+    else if (n == 2) {
+        cout << "two";
+    }
+    else if (n == 3) {
+        cout << "three";
+    }
+    else if (n == 4) {
+        cout << "four";
+    }
+    else if (n == 5) {
+        cout << "five";
+    }
+    else if (n == 6) {
+        cout << "six";
+    }
+    else if (n == 7) {
+        cout << "seven";
+    }
+    else if (n == 8) {
+        cout << "eight";
+    }
+    else if (n == 9) {
+        cout << "nine";
+    } */
     return 0;
 }
