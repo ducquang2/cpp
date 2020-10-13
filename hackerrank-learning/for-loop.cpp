a6fde83a6d6bbe3b3da698656eced28efe717428
ducquang2 <quangbilun@gmail.com>
2020-10-13 11:50:31 +0700

    first-condition

--- hackerrank-learning/for-loop.cpp
+++ hackerrank-learning/for-loop.cpp
@@ -7,5 +7,10 @@ int main() {
     int i = 0;
     cin >> a >> b;
     string num[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    if ((a < b) && (b <= 9)) {
+        for (i = a; i <= b; i++) {
+            cout << num[i - 1] << endl;
+        }
+    }
     return 0;
 }
