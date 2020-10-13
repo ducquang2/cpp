5a6a5a1377d25710f777f1ad43f96805cb507d65
ducquang2 <quangbilun@gmail.com>
2020-10-13 11:51:18 +0700

    second-condition

--- hackerrank-learning/for-loop.cpp
+++ hackerrank-learning/for-loop.cpp
@@ -12,5 +12,18 @@ int main() {
             cout << num[i - 1] << endl;
         }
     }
+    else if ((a < b) && (b > 9)) { 
+        for (i = a; i <= b; i++) {
+            cout << num[i - 1] << endl;
+        }
+        for (i = 10; i <= b; i++) {
+            if (i % 2 == 0) {
+                cout << "even" << endl;
+            }
+            else { 
+                cout << "odd" << endl;
+            }
+        }
+    }
     return 0;
 }
