32b971996905d0630c0f25962488b02b3335d9ee
ducquang2 <quangbilun@gmail.com>
2020-10-13 10:52:25 +0700

    printf

--- hackerrank-learning/basic-data-types.cpp
+++ hackerrank-learning/basic-data-types.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
     int i; long l; char c; float f; double d;
     scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
+    printf("%d\n %ld\n %c\n %f\n %lf", &i, &l, &c, &f, &d);
     return 0;
 }
