f14b72c6f1487d9325b5c57ea4454f30fb21222e
ducquang2 <quangbilun@gmail.com>
2020-10-13 10:53:49 +0700

    second-method

--- hackerrank-learning/basic-data-types.cpp
+++ hackerrank-learning/basic-data-types.cpp
@@ -2,9 +2,12 @@
 #include <cstdio>
 using namespace std;
 
+// first method
 int main() {
     int i; long l; char c; float f; double d;
     scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
     printf("%d\n%ld\n%c\n%f\n%lf", i, l, c, f, d);
     return 0;
 }
+
+// second method
