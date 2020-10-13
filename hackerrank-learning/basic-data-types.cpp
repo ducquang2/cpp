096f110375c29f42402ea40ee3a980055cc93ea3
ducquang2 <quangbilun@gmail.com>
2020-10-13 10:58:47 +0700

    second-method

--- hackerrank-learning/basic-data-types.cpp
+++ hackerrank-learning/basic-data-types.cpp
@@ -10,4 +10,16 @@ int main() {
     return 0;
 }
 
-// second method
+/* second method but suck
+int main() {
+    int i; long l; char c; float f; double d;
+    cin >> i >> l >> c >> f >> d;
+    cout << i << endl;
+    cout << l << endl;
+    cout << c << endl;
+    cout.precision(3);
+    cout << fixed << f << endl;
+    cout.precision(9);
+    cout << fixed << d << endl;
+    return 0;
+} */
