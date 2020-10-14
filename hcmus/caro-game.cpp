8a6aefab9b36464da0f1c5004b804b4202bcab8c
ducquang2 <quangbilun@gmail.com>
2020-10-14 20:02:28 +0700

    update

--- hcmus/caro-game.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <cstdio>
-using namespace std;
-
-void FixConsoleWindow() {
-    HWND consoleWindow = GetConsoleWindow();
-    LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
-}
