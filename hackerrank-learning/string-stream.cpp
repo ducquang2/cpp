09949d4b2899406da4c02ce475c18200a3742241
ducquang2 <quangbilun@gmail.com>
2020-10-21 17:40:43 +0700

    stringstream

--- hackerrank-learning/string-stream.cpp
+++ hackerrank-learning/string-stream.cpp
@@ -6,6 +6,7 @@ using namespace std;
 vector<int> parseInts(string str) {
 	// Complete this function
     vector<int> vec; // Declares a vector to store the ints
+    stringstream ss(str); // Declares a stringstream object to deal with the modification of the string
 }
 
 int main() {
