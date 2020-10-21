80b58a8caea482301d0204f1ae1febeb356c0b77
ducquang2 <quangbilun@gmail.com>
2020-10-21 17:42:20 +0700

    loop

--- hackerrank-learning/string-stream.cpp
+++ hackerrank-learning/string-stream.cpp
@@ -7,6 +7,15 @@ vector<int> parseInts(string str) {
 	// Complete this function
     vector<int> vec; // Declares a vector to store the ints
     stringstream ss(str); // Declares a stringstream object to deal with the modification of the string
+    char ch;
+    int temp;
+
+    while (ss) // While the stringstream object does not hit a null byte
+    {
+        ss >> temp >> ch; // Extract the comma seperated ints with the extraction >> operator
+        vec.push_back(temp);   // Push the int onto the vector
+    }
+    
 }
 
 int main() {
