9bbb7ccbd896aee8ba7f9a4a3f0b2f77adc61c42
ducquang2 <quangbilun@gmail.com>
2020-10-25 17:45:20 +0700

    second-way

--- hackerrank-learning/strings.cpp
+++ hackerrank-learning/strings.cpp
@@ -2,6 +2,7 @@
 #include <string>
 using namespace std;
 
+// First way to solve
 int main() {
     string a, b;
     cin >> a >> b;
@@ -15,3 +16,23 @@ int main() {
 
     return 0;
 }
+
+/* Second way to solve
+int main() {
+    string a, b;
+    char temp;
+    cin >> a >> b;
+    
+    cout << a.size() << " " << b.length() << endl;
+    
+    cout << a + b << endl;
+    
+    temp = a[0];
+    a[0] = b[0];
+    b[0] = temp;
+    cout << a << " " << b << endl;
+
+    return 0;
+}
+*/
+
