16469a066efde68ffc4079ab80158abe77dee695
ducquang2 <quangbilun@gmail.com>
2020-10-25 17:35:03 +0700

    done

--- hackerrank-learning/strings.cpp
+++ hackerrank-learning/strings.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main() {
+    string a, b;
+    cin >> a >> b;
 
+    cout << a.length() << " " << b.size() << endl;
+    
+    cout << a + b << endl;
+
+    swap(a[0], b[0]);
+    cout << a << " " << b << endl;
+
+    return 0;
 }
