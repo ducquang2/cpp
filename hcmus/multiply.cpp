2730ae2302da6473d41474e76dfc7e4c5361e53f
ducquang2 <quangbilun@gmail.com>
2020-10-27 16:16:33 +0700

    hcmus

--- hcmus/multiply.cpp
+++ hcmus/multiply.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 int main() {
-    int number, multi = 1;
+    int n, multi = 1;
 
     cout << "Nhap so n: ";
+    cin >> n;
+
+    while (n > 0) {
+        multi *= (n % 10);
+        n /= 10;
+    }
+
+    cout << endl << "Tich cac ky so la: " << multi;
+
+    return 0;
 }
